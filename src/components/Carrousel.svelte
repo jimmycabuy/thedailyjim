f67c16f53c82952ced00d246b65f00c3db7dfcd5
Jimmy Cabuy <jimmy.cabuy@decathlon.com>
2023-08-28 14:53:53 +0200

    refactor + component

---
+++ src/components/Carrousel.svelte
@@ -0,0 +1,117 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  export let carouselShopPhotos, indexCarousel, folder;
+</script>
+
+<div class="block_carousel">
+  <div class="block_image_carousel">
+    <button on:click={() => dispatch("previousCarrousel")} class="btn_carrou">
+      <img
+        class="arrow"
+        src="../../../assets/asset/previous.png"
+        alt="previous"
+        preload
+      />
+    </button>
+    {#each [carouselShopPhotos[indexCarousel]] as photo}
+      <div class="block_image">
+        <img
+          class="image_carousel"
+          src={`../../../assets/${folder}/${photo}.webp`}
+          alt={photo}
+          preload
+        />
+      </div>
+    {/each}
+    <button on:click={() => dispatch("nextCarrousel")} class="btn_carrou">
+      <img
+        class="arrow"
+        src="../../../assets/asset/next.png"
+        alt="next"
+        preload
+      />
+    </button>
+  </div>
+</div>
+
+<style>
+  img {
+    max-height: 80vh;
+    object-fit: contain;
+    pointer-events: none;
+  }
+  .block_carousel {
+    padding-left: 2rem;
+    padding-right: 2rem;
+  }
+  .block_image_carousel {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 100%;
+  }
+  .btn_carrou {
+    height: 30px;
+    width: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    background: none;
+  }
+  .arrow {
+    height: 30px;
+    width: 30px;
+  }
+  .btn_carrou:hover {
+    cursor: pointer;
+  }
+
+  button:disabled {
+    cursor: not-allowed;
+    pointer-events: all !important;
+  }
+
+  @media (max-width: 767px) {
+    .image_carousel {
+      max-width: 80vw;
+      max-height: 80vh;
+      object-fit: contain;
+    }
+    .block_carousel {
+      height: 60vh;
+      align-items: center;
+      display: flex;
+      justify-content: center;
+      padding-left: 0rem;
+      padding-right: 0rem;
+    }
+  }
+  @media (min-width: 767px) and (max-width: 1023px) {
+    .image_carousel {
+      max-width: 80vw;
+      max-height: 80vh;
+      object-fit: contain;
+    }
+  }
+  /* tablet portrait  */
+  @media (min-width: 767px) and (max-width: 1023px) and (orientation: portrait) {
+    .image_carousel {
+      width: 90vw;
+      max-height: 70vh;
+      object-fit: contain;
+    }
+
+    .block_carousel {
+      height: 70vh;
+      align-items: center;
+      display: flex;
+      justify-content: center;
+      padding-left: 0rem;
+      padding-right: 0rem;
+    }
+  }
+</style>
---
+++ src/components/NavigationBack.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let folder;
+</script>
+
+<div>
+  <a href={folder === "home" ? "/" : "/" + folder} class="go_back">
+    <img
+      src="../../../assets/asset/previous.png"
+      alt="previous"
+      class="arrow_back"
+      preload
+    />
+    {folder.toUpperCase()}
+  </a>
+</div>
+
+<style>
+  .go_back {
+    font-family: "bd-supper", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    display: flex;
+    align-items: center;
+    margin-left: -5px;
+    width: fit-content;
+    margin-bottom: 1rem;
+    margin-top: 1rem;
+    padding-left: 1rem;
+  }
+  .arrow_back {
+    height: 20px;
+    width: 20px;
+    padding-bottom: 3px;
+  }
+</style>
